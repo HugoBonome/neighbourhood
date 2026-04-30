1b271158cecac9c596f277639a40b14c421fee91
hugo <hugo.zb@aluno.ifsc.edu.br>
2026-04-30 19:40:13 -0300

    feat: criando os arquivos iniciais das telas do app

---
+++ app/_layout.tsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from 'react'
+import { Slot, useRouter, useSegments } from 'expo-router'
+import { supabase } from '@/lib/supabase.ts'
+import { Session } from '@supabase/supabase-js'
+
+export default function RootLayout() {
+  const [session, setSession] = useState<Session | null>(null)
+  const [loading, setLoading] = useState(true)
+  const router = useRouter()
+  const segments = useSegments()
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+      setLoading(false)
+    })
+
+    const { data: { subscription } } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+
+    return () => subscription.unsubscribe()
+  }, [])
+
+  useEffect(() => {
+    if (loading) return
+
+    const inAuthGroup = segments[0] === '(auth)'
+
+    if (!session && !inAuthGroup) {
+      router.replace('/(auth)/login')
+    } else if (session && inAuthGroup) {
+      router.replace('/(app)/feed')
+    }
+  }, [session, loading, segments])
+
+  return <Slot />
+}
